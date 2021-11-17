f061eeb42b3f35554f8594c0cea673779af8f1d7
falanter <1654551076@qq.com>
2021-11-17 17:46:28 +0800

    Money.tsx 中 NoteSection UI 完成

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -24,7 +24,26 @@ const TagSection=styled.section`
     cursor: pointer;
   }
 `
-const NotesSection=styled.section``
+const NotesSection=styled.section`
+  background: #f5f5f5;
+  padding: 10px 16px;
+  font-size: 14px;
+  label{
+    display: flex;
+    align-items: center;
+    span{
+      margin-right:16px;
+      white-space: nowrap;
+    }
+    input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`
 const CategorySection=styled.section``
 const NumberPadSection=styled.section``
 
@@ -43,7 +62,7 @@ function Tags(){
         <NotesSection>
           <label>
             <span>备注</span>
-            <input type="text" />
+            <input type="text" placeholder="在这里添加备注" />
           </label>
         </NotesSection>
         <CategorySection>
