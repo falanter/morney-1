c6e381704969ff776a702a189cd4758860917033
falanter <1654551076@qq.com>
2021-11-18 17:58:20 +0800

    CategorySection.tsx切换功能实现

--- src/view/Money/CategorySection.tsx
+++ src/view/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const CategorySection=styled.section`
+const Wrapper=styled.section`
   font-size: 24px;
   ul{
     display:flex;
@@ -23,4 +24,25 @@ const CategorySection=styled.section`
     }
   }
 `
+const CategorySection:React.FC=(props)=>{
+  const categoryMap={'-':'支出','+':'收入'};
+  type Keys = keyof typeof categoryMap;        //封装
+  const [categoryList] = useState<Keys[]>(['-','+']);
+  // const [categoryList] = useState<('-' | '+')[]>(['-','+']);  //参数类型只能选择'+'和'-'
+  const [category,setCategory]=useState('-');  //+表示收入 -表示支出
+  return(
+    <Wrapper>
+      <ul>
+        {categoryList.map(c=>
+          <li 
+            key={c}
+            className={category===c ? 'selected' : ''}
+            onClick={()=>{setCategory(c);}}
+          >{categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper>
+  );
+}
 export {CategorySection};
