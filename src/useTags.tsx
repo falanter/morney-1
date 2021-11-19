6ac7e899d355737a933e222774fda7df12d47116
falanter <1654551076@qq.com>
2021-11-19 17:41:42 +0800

    tags 样式完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const useTags=()=>{
+const useTags=()=>{     //规定为用use..//封装一个自定义Hook
     const [tags,setTags]=useState<string[]>(['衣','食','住','行']);
     return{tags,setTags}
 }
--- src/view/Tags.tsx
+++ src/view/Tags.tsx
@@ -1,11 +1,64 @@
 import Layout from "components/Layout";
+import styled from "styled-components";
 import { useTags } from "useTags";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background-color: white;
+  li{
+    border-bottom: 1px solid #d5d5d5;
+    line-height: 20px;
+    padding: 12px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .arrow-right::before{
+        content:'';
+        display: block;
+        width: 6px;
+        height: 6px;
+        border-left: 1px solid #000;
+        border-top:1px solid #000;
+        transform: rotate(135deg);
+    }
+  }
+`
+const Button=styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background-color: #999;
+  border-radius: 4px;
+  color: #fff;
+`
+const Center=styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Space=styled.div`
+  height: 16px;
+`
 
 function Tags(){
   const {tags,setTags}=useTags();
     return(
       <Layout>
-        <h2>tags</h2>
+        <TagList>
+          {tags.map(tag=>
+            <li key={tag}>
+              <span className="oneLine">{tag}</span>
+              <div className="arrow-right"></div>
+            </li>
+          )}
+        </TagList>
+        <Center>
+          <Space />
+          <Space />
+          <Button>新增标签</Button>
+          <Space />
+        </Center>
       </Layout>
     );
 }
